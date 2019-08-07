44dd03414222bc7fa45d76f27a62cf52ac9bac23
Wílmer <42726764+wilmereleon@users.noreply.github.com>
2019-08-07 10:55:17 -0500

    Create methods of Multiplication and Division

--- Menu/src/Menu.java
+++ Menu/src/Menu.java
@@ -18,14 +18,28 @@ public class Menu {
 				numberEnteredA = mk.nextDouble();
 				System.out.println("Type second number: ");
 				numberEnteredB = mk.nextDouble();
-				System.out.println (getAdd(numberEnteredA, numberEnteredB));
+				System.out.println (getAdd (numberEnteredA, numberEnteredB));
 				break;
 			case 2:
 				System.out.println("Type first number: ");
 				numberEnteredA = mk.nextDouble();
 				System.out.println("Type second number: ");
 				numberEnteredB = mk.nextDouble();
-				System.out.println (getSubtraction(numberEnteredA, numberEnteredB));
+				System.out.println (getSubtraction (numberEnteredA, numberEnteredB));
+				break;
+			case 3:
+				System.out.println("Type first number: ");
+				numberEnteredA = mk.nextDouble();
+				System.out.println("Type second number: ");
+				numberEnteredB = mk.nextDouble();
+				System.out.println (getMultiplication (numberEnteredA, numberEnteredB));
+				break;
+			case 4:
+				System.out.println("Type first number: ");
+				numberEnteredA = mk.nextDouble();
+				System.out.println("Type second number: ");
+				numberEnteredB = mk.nextDouble();
+				System.out.println (getDivision (numberEnteredA, numberEnteredB));
 				break;
 			default:
 				System.err.println("Error");
@@ -43,10 +57,20 @@ public class Menu {
 		return numberA - numberB;
 	}
 	
+	public static double getMultiplication (double numberA, double numberB) {
+		return numberA * numberB;
+	}
+	
+	public static double getDivision (double numberA, double numberB) {
+		return numberA / numberB;
+	}
+	
 	private static void showMenuResult () {
 		System.out.println ("(0) Exit");
 		System.out.println ("(1) Addition");
 		System.out.println ("(2) Subtraction");
+		System.out.println ("(3) Multiplication");
+		System.out.println ("(4) Division");
 	}
 	
 }
