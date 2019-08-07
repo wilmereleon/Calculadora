9d28e6d6e0bab22bc6b50b25c64ef33c1476ed91
Wílmer <42726764+wilmereleon@users.noreply.github.com>
2019-08-07 12:07:59 -0500

    Methods of Root and Pow

--- Menu/src/Menu.java
+++ Menu/src/Menu.java
@@ -19,7 +19,7 @@ public class Menu {
 				System.out.println("Type second number: ");
 				numberEnteredB = mk.nextDouble();
 				System.out.println (getAdd (numberEnteredA, numberEnteredB));
-				System.out.println ("End Program");
+				System.out.println ("«End Program»");
 				System.out.println ("");
 				break;
 			case 2:
@@ -28,7 +28,7 @@ public class Menu {
 				System.out.println("Type second number: ");
 				numberEnteredB = mk.nextDouble();
 				System.out.println (getSubtraction (numberEnteredA, numberEnteredB));
-				System.out.println ("End Program");
+				System.out.println ("«End Program»");
 				System.out.println ("");
 				break;
 			case 3:
@@ -37,7 +37,7 @@ public class Menu {
 				System.out.println("Type second number: ");
 				numberEnteredB = mk.nextDouble();
 				System.out.println (getMultiplication (numberEnteredA, numberEnteredB));
-				System.out.println ("End Program");
+				System.out.println ("«End Program»");
 				System.out.println ("");
 				break;
 			case 4:
@@ -46,7 +46,7 @@ public class Menu {
 				System.out.println("Type second number: ");
 				numberEnteredB = mk.nextDouble();
 				System.out.println (getDivision (numberEnteredA, numberEnteredB));
-				System.out.println ("End Program");
+				System.out.println ("«End Program»");
 				System.out.println ("");
 				break;
 			case 5:
@@ -59,15 +59,33 @@ public class Menu {
 
 				int d = getDiscriminant(numA, numB, numC);
 				if (d < 0) {
-					System.out.println("No real solution.");
+					System.out.println ("No real solution.");
 				}
 				else {
-				double x1 = (-numB + Math.sqrt (d))/(2 * numA);
-				double x2 = (-numB - Math.sqrt (d))/(2 * numA);
-				System.out.println("Root 1: "+x1);
-				System.out.println("Root 2: "+x2);
+				double x1 = (-numB + Math.sqrt (d)) / (2 * numA);
+				double x2 = (-numB - Math.sqrt (d)) / (2 * numA);
+				System.out.println ("Root 1: " + x1);
+				System.out.println ("Root 2: " + x2);
 				}
-				System.out.println ("End Program");
+				System.out.println ("«End Program»");
+				System.out.println ("");
+				break;
+			case 6:
+				System.out.println("Type the radicand: ");
+				numberEnteredA = mk.nextDouble();
+				System.out.println("Type the index: ");
+				numberEnteredB = mk.nextDouble();
+				System.out.println (getRoot (numberEnteredA, numberEnteredB));
+				System.out.println ("«End Program»");
+				System.out.println ("");
+				break;
+			case 7:
+				System.out.println("Type the radicand: ");
+				numberEnteredA = mk.nextDouble();
+				System.out.println("Type the index: ");
+				numberEnteredB = mk.nextDouble();
+				System.out.println (getPow (numberEnteredA, numberEnteredB));
+				System.out.println ("«End Program»");
 				System.out.println ("");
 				break;
 			default:
@@ -94,13 +112,20 @@ public class Menu {
 		return numberA / numberB;
 	}
 	
-		public static int getDiscriminant (int numA, int numB, int numC) {
-			int answer = 0;
-			answer = numB * numB - 4 * numA * numC;
-			return answer;
+	public static int getDiscriminant (int numA, int numB, int numC) {
+		int answer = 0;
+		answer = numB * numB - 4 * numA * numC;
+		return answer;
+	}
+	
+	public static double getRoot (double numberA, double numberB) {
+		return Math.pow (numberA, 1 / numberB);
+	}
+	
+	public static double getPow (double numberA, double numberB) {
+		return Math.pow (numberA, numberB);
 	}
 
-	
 	private static void showMenuResult () {
 		System.out.println ("(0) Exit");
 		System.out.println ("(1) Addition");
@@ -108,6 +133,8 @@ public class Menu {
 		System.out.println ("(3) Multiplication");
 		System.out.println ("(4) Division");
 		System.out.println ("(5) Quadratic");
+		System.out.println ("(6) Root");
+		System.out.println ("(7) Pow");
 	}
 	
 }
