af1780f93ec464920afcd36648458b38a6365280
Wílmer <42726764+wilmereleon@users.noreply.github.com>
2019-09-02 09:35:32 -0500

    Example

---
+++ IntroAArreglos/src/ArraysOneWeek.java
@@ -0,0 +1,24 @@
+
+public class ArraysOneWeek {
+
+	public static void main(String[] args) {
+		String pArray2 [] = {"1", "8", "0", "4"};
+		String pArray1 [] = {"4", "9", "13", "2"};
+		
+		String[] result = concatenar(pArray1, pArray2);
+		
+		for (int i = 0; i < result.length; i ++) {
+			System.out.println (result[i]);
+		}
+
+	}
+	public static String[] concatenar(String[] pArray1, String[] pArray2) {
+		String[] result = new String [pArray1.length + pArray1.length];
+		
+		System.arraycopy(pArray1, 0, result, 0, pArray1.length);
+		System.arraycopy(pArray2, 0, result, pArray1.length, pArray2.length);
+		return result;
+		
+	}
+
+}
