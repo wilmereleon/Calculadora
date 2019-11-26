286db6de42504c866e479da259a22f0367722400
Wílmer <42726764+wilmereleon@users.noreply.github.com>
2019-11-26 14:17:32 -0500

    Button's event

--- Orthographic_Shunt_With_JavaFX/src/application/Main.java
+++ Orthographic_Shunt_With_JavaFX/src/application/Main.java
@@ -5,16 +5,27 @@ import com.sun.prism.Image;
 import javafx.application.Application;
 import javafx.scene.control.Button;
 import javafx.fxml.FXMLLoader;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
+//import javafx.scene.layout.BorderPane;
+//import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 
-public class Main extends Application {
+public class Main extends Application implements EventHandler<ActionEvent>{
 	
+	@Override
+	public void handle(ActionEvent event) {
+		if(event.getSource()==button) {
+		System.out.println();	
+		}
+		
+	}
 	Button button;
+	
+	
 	//@Override
 	//public void start(Stage primaryStage) {
 		//try {
@@ -34,19 +45,26 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
+	@Override
 	public void start(Stage primaryStage) throws Exception {
+		
+		
 		primaryStage.setTitle("Ortografía por derivación");
 		
 		button = new Button();
 		button.setText("Iniciar");
+	
+		button.setOnAction(this);
 		
 		StackPane layout = new StackPane ();
 		layout.getChildren().add(button);
 		
-		Scene scene = new Scene(layout, 300, 250);
+		Scene scene = new Scene(layout, 500, 350);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
 	}
+	
+	
+	
 }
