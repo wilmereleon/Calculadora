2cb7334c85ca084e39861fe1c7990f207b628642
Wílmer <42726764+wilmereleon@users.noreply.github.com>
2019-11-26 10:05:02 -0500

    Prove code

--- Orthographic_Shunt_With_JavaFX/src/application/Main.java
+++ Orthographic_Shunt_With_JavaFX/src/application/Main.java
@@ -1,27 +1,45 @@
 package application;
 	
+import com.sun.prism.Image;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
 	
+	Button button;
+	//@Override
+	//public void start(Stage primaryStage) {
+		//try {
+			//Image img = new Image("application/Logo.png");
+			//ImageView imgView = new ImageView(img);
+			//BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			//root.setCenter(imgView);
+			//Scene scene = new Scene(root,400,400);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			//primaryStage.setScene(scene);
+			//primaryStage.show();
+		//} catch(Exception e) {
+			//e.printStackTrace();
+		//}
+	//}
+	//
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Ortografía por derivación");
+		
+		button = new Button();
+		button.setText("Iniciar");
+		
+		
 	}
 }
