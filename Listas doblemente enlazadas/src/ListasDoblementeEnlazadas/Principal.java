da2df6e114fb2bcda40176d52e4f03ffa852343d
Wílmer <42726764+wilmereleon@users.noreply.github.com>
2021-02-25 10:36:14 -0500

    Se crean los métodos de agregarALaCola y verificarListaVacia: agrega elementos a la cola y verifica si la lista está vacía, respectivamente

--- Listas doblemente enlazadas/src/ListasDoblementeEnlazadas/Principal.java
+++ Listas doblemente enlazadas/src/ListasDoblementeEnlazadas/Principal.java
@@ -8,4 +8,20 @@ public class Principal {
 		cabeza = null;
 		cola = null;
 	}
+	
+	public void agregarALaCola (int x) { // Agrega nodos al final de la lista
+		if (!verificarListaVacia()) { // Verifica si el método verificarListaVacia está efectivamente vacía (!: símbolo para verificar métodos)
+			cola = new Nodo (x, cola, null); // Recibe datos del constructor de 3 parámetros: «x», cola paunta a null, y cabeza va a cola
+			cola.punteroAnterior.punteroSiguiente = cola;
+		}
+		else { // De no cumplirse la sentencia anterior, va al constructor vacío
+			cabeza = new Nodo (x);
+			cola = new Nodo (x);
+		}
+	}
+	
+	
+	public boolean verificarListaVacia () { // Verifica si la lista está vacía
+		return (cabeza == null); // Solop se necesita verificar uno de los estados para estimar si la lista está acia; se usa la cabeza
+	}
 }
