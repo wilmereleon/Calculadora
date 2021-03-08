883d86fe01097e220e7b1811be997f5384c8601c
Wílmer <42726764+wilmereleon@users.noreply.github.com>
2021-03-07 20:48:42 -0500

    Se crea la clase Nodo; se extienden propiedades de la librería «Applet»

---
+++ Lista de supermercado/src/Lista/Nodo.java
@@ -0,0 +1,5 @@
+package Lista;
+
+public class Nodo {
+
+}
---
+++ Lista de supermercado/src/Lista/Principal.java
@@ -0,0 +1,14 @@
+package Lista;
+
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+
+/*
+ * @ author: Wílmer E, León Díaz
+ * Lista de supermercado usando listas doblemente enlazadas
+ * 
+ */
+public class Principal extends Applet { // Se extienden características de la librería «Applet»
+	   
+}
