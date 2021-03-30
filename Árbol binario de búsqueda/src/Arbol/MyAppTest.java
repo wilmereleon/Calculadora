681c612a238d5e1c33015d1c6cb59fe0ab42600a
Wílmer <42726764+wilmereleon@users.noreply.github.com>
2021-03-29 23:24:14 -0500

    Creación de la clase de pruebas

---
+++ Árbol binario de búsqueda/src/Arbol/MyAppTest.java
@@ -0,0 +1,39 @@
+package Arbol;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MyAppTest {
+
+	@Test
+	void testMyApp() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testInsercion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testInorden() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPreorden() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPosorden() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
